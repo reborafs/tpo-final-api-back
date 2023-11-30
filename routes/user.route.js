02f6e725cdf3a1945242f2bc9c5eb9af47101122
reborafs <reborafs@gmail.com>
2023-11-30 20:06:24 -0300

    agrego autenticacion

--- routes/user.route.js
+++ routes/user.route.js
@@ -12,9 +12,11 @@ router.get('/', function(req, res, next) {
 
 router.post('/registration', UserController.createUser)
 router.post('/login/', UserController.loginUser)
+router.post('/image-upload', UserController.uploadImage)
 router.get('/users', UserController.getUsers)
 //router.get('/users',Authorization, UserController.getUsers)
 router.get('/userByMail', Authorization, UserController.getUsersByMail)
+router.get('/userById', UserController.getUserById)
 router.put('/update', Authorization, UserController.updateUser)
 //router.delete('/delete', Authorization, UserController.removeUser)
 router.delete('/delete',  UserController.removeUser)
